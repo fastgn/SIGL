6bae95076cd69f0baa8f4c1fe9a09c285fb0871f
HarryOnBreadboard <a.leroux330@laposte.net>
2024-10-01 16:31:35 +0200

    Ajout de tailwindCSS

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -17,14 +17,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" className="flex justify-center">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" className="flex justify-center">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-4xl">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
