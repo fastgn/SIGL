0d0090ae36dbefb0bccb8a09f28a308abcc236f9
florian_G <florian.gouverneur@reseau.eseo.fr>
2024-11-05 14:46:23 +0100

    add test, rebase model into controller

--- apps/server/src/controllers/diaryController.ts
+++ apps/server/src/controllers/diaryController.ts
@@ -1,27 +1,78 @@
-import { Request, Response } from "express";
 import trainingDiaryModel from "../models/trainingDiary";
+import { ControllerError, ControllerSuccess } from "../utils/controller";
+import { db } from "../providers/db";
 
 const diaryController = {
-  createDiary: async (req: Request, res: Response) => {
-    const { user_id } = req.body;
+  createDiary: async (user_id: number) => {
+    //const { user_id } = req.body;
 
     if (!user_id) {
-      return res
-        .status(400)
-        .json({ message: "Veuillez renseigner le user pour associer le journal" });
+      return ControllerError.INVALID_PARAMS({ message: "user_id est requis" });
     }
 
-    if (typeof user_id !== "string") {
-      return res.status(400).json({ message: "Le user doit être une chaîne de caractères" });
+    if (typeof user_id !== "number") {
+      return ControllerError.INVALID_PARAMS({ message: "user_id doit être un nombre" });
+    }
+    const checkUserExist = await db.user.findFirst({
+      where: {
+        id: user_id,
+      },
+    });
+
+    if (checkUserExist === null) {
+      return ControllerError.INVALID_PARAMS({ message: "L'utilisateur n'existe pas" });
     }
 
-    const diary = await trainingDiaryModel().insertNewDiary(parseInt(user_id as string));
+    const diary = await db.trainingDiary.create({
+      data: {
+        description: "Journal de formation",
+        event: [],
+        deliverable: [],
+        apprenticeId: user_id,
+      },
+    });
 
     if (!diary) {
-      return res.status(500).json({ message: "Erreur lors de la création du journal" });
+      return ControllerError.INTERNAL({ message: "Erreur lors de la création du journal" });
     }
 
-    return res.status(201).json(diary);
+    return ControllerSuccess.SUCCESS({ message: "Journal créé avec succès", data: diary });
+  },
+  deleteDiary: async (user_id: number) => {
+    //const { user_id } = req.body;
+
+    if (!user_id) {
+      return ControllerError.INVALID_PARAMS({ message: "user_id est requis" });
+    }
+
+    if (typeof user_id !== "number") {
+      return ControllerError.INVALID_PARAMS({ message: "user_id doit être un nombre" });
+    }
+
+    const checkUserExist = await db.user.findFirst({
+      where: {
+        id: user_id,
+      },
+    });
+
+    if (checkUserExist === null) {
+      return ControllerError.INVALID_PARAMS({ message: "L'utilisateur n'existe pas" });
+    }
+
+    const deleteDiary = await db.trainingDiary.delete({
+      where: {
+        apprenticeId: user_id,
+      },
+    });
+
+    if (!deleteDiary) {
+      return ControllerError.INTERNAL({ message: "Erreur lors de la création du journal" });
+    }
+
+    return ControllerSuccess.SUCCESS({
+      message: "Journal supprimer avec succès",
+      data: deleteDiary,
+    });
   },
 };
 
--- apps/server/src/routes/diaryRoutes.ts
+++ apps/server/src/routes/diaryRoutes.ts
@@ -1,7 +1,17 @@
 import express from "express";
 import diaryController from "../controllers/diaryController";
+import { reply } from "../utils/http";
+import { ControllerError } from "../utils/controller";
 const router = express.Router();
 
-router.post("/", diaryController.createDiary);
-
+router.post("/", async (req, res) => {
+  try {
+    const { user_id } = req.body;
+    const result = await diaryController.createDiary(parseInt(user_id));
+    reply(res, result);
+  } catch (error) {
+    console.error(error);
+    reply(res, ControllerError.INTERNAL());
+  }
+});
 export default router;
