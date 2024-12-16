95f8b1d2c6c0c6f409db26754c4a0496a48ac1fd
Timothée <timothee.viardtv@gmail.com>
2024-12-16 15:49:17 +0100

    Refacto sur la page utilisateur

--- apps/client/src/components/features/users/user/UserInfoPage.tsx
+++ apps/client/src/components/features/users/user/UserInfoPage.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { ArrowLeft, Edit } from "lucide-react";
+import { ArrowLeft } from "lucide-react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Banner } from "@/components/common/banner/Banner";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -12,6 +11,7 @@ import { UserTypeReq } from "@/components/features/users/UsersPage";
 import { UpdateIcon } from "@radix-ui/react-icons";
 import { useUser } from "@/contexts/UserContext";
 import { FormChangePassword } from "@/components/features/users/user/FormChangePassword";
+import Bloc from "@/components/common/bloc/bloc";
 
 export interface User {
   id: number;
@@ -27,6 +27,7 @@ export interface User {
 }
 
 export const UserDetailsPage = () => {
+  const noEditFields = "bg-blue-10 border-blue-10 cursor-not-allowed";
   const [user, setUser] = useState<User | null>(null);
   const [isEditing, setIsEditing] = useState(false);
   const [editedUser, setEditedUser] = useState<User | null>(null);
@@ -78,41 +79,40 @@ export const UserDetailsPage = () => {
     setEditedUser((prev) => (prev ? { ...prev, [name]: value } : null));
   };
 
+  const userInfoActions = !isEditing ? (
+    <Button variant={"empty"} onClick={handleEdit} className="p-2">
+      <PencilIcon className="h-5 w-5" />
+    </Button>
+  ) : (
+    <div>
+      <Button variant={"empty"} onClick={handleSave} className="p-2">
+        <Check className="h-5 w-5" />
+      </Button>
+      <Button variant={"empty"} onClick={handleCancel} className="p-2">
+        <X className="h-5 w-5" />
+      </Button>
+    </div>
+  );
+
   return (
     <div className="flex flex-col min-h-screen">
       <Banner />
       <div className="flex flex-col gap-5 px-16 py-12">
         <div className="flex justify-between items-center gap-3">
           <h1 className="text-3xl font-bold">Edition d'utilisateur</h1>
-          <Button onClick={() => navigate(-1)} variant="outline">
-            <ArrowLeft className="mr-2 h-4 w-4" />
-            Quitter
+          <Button variant={"add"} onClick={undefined}>
+            Créer le journal de formation
           </Button>
         </div>
         {user && editedUser ? (
-          <div className="flex flex-col gap-5">
-            <Card className="w-full">
-              <CardHeader>
-                <CardTitle className="flex justify-between items-center text-xl">
-                  Informations utilisateur
-                  {!isEditing ? (
-                    <Button onClick={handleEdit}>
-                      <Edit className="mr-2 h-4 w-4" />
-                      Editer
-                    </Button>
-                  ) : (
-                    <div>
-                      <Button onClick={handleSave} className="mr-2">
-                        Save
-                      </Button>
-                      <Button onClick={handleCancel} variant="outline">
-                        Cancel
-                      </Button>
-                    </div>
-                  )}
-                </CardTitle>
-              </CardHeader>
-              <CardContent className="space-y-6">
+          <div className="flex flex-row gap-5">
+            <div className="w-3/5">
+              <Bloc
+                title="Informations utilisateur"
+                actions={userInfoActions}
+                defaultOpen
+                isOpenable
+              >
                 <div className="flex justify-center">
                   <Avatar className="w-24 h-24 rounded-full">
                     <AvatarImage
@@ -136,6 +136,7 @@ export const UserDetailsPage = () => {
                       value={editedUser.lastName}
                       onChange={handleInputChange}
                       readOnly={!isEditing}
+                      className={!isEditing ? noEditFields : ""}
                     />
                   </div>
                   <div className="space-y-2">
@@ -146,6 +147,7 @@ export const UserDetailsPage = () => {
                       value={editedUser.firstName}
                       onChange={handleInputChange}
                       readOnly={!isEditing}
+                      className={!isEditing ? noEditFields : ""}
                     />
                   </div>
                   <div className="space-y-2">
@@ -156,6 +158,7 @@ export const UserDetailsPage = () => {
                       value={editedUser.email}
                       onChange={handleInputChange}
                       readOnly={!isEditing}
+                      className={!isEditing ? noEditFields : ""}
                     />
                   </div>
                   <div className="space-y-2">
@@ -166,6 +169,7 @@ export const UserDetailsPage = () => {
                         name="password"
                         value={editedUser.password}
                         type="password"
+                        className={noEditFields}
                         readOnly
                       />
                       <FormChangePassword user={editedUser} isAdmin={isAdmin} />
@@ -179,6 +183,7 @@ export const UserDetailsPage = () => {
                       value={editedUser.role}
                       onChange={handleInputChange}
                       readOnly={!isEditing}
+                      className={!isEditing ? noEditFields : ""}
                     />
                   </div>
                   <div className="space-y-2">
@@ -189,6 +194,7 @@ export const UserDetailsPage = () => {
                       value={new Date(editedUser.birthDate).toLocaleDateString("fr-FR")}
                       onChange={handleInputChange}
                       readOnly={!isEditing}
+                      className={!isEditing ? noEditFields : ""}
                     />
                   </div>
                   <div className="space-y-2">
@@ -199,26 +205,28 @@ export const UserDetailsPage = () => {
                       value={editedUser.phone}
                       onChange={handleInputChange}
                       readOnly={!isEditing}
+                      className={!isEditing ? noEditFields : ""}
                     />
                   </div>
                 </div>
-              </CardContent>
-            </Card>
+              </Bloc>
+            </div>
 
-            {editedUser.role.map((role) => (
-              <Card key={role} className="w-full">
-                <CardHeader>
-                  <CardTitle className="flex justify-between items-center text-xl">
-                    <div>Rôle: {role}</div>
-                  </CardTitle>
-                </CardHeader>
-                <CardContent></CardContent>
-              </Card>
-            ))}
+            <div className="flex-auto">
+              {editedUser.role.map((role) => (
+                <Bloc title={`Rôle: ${role}`} actions={undefined} isOpenable></Bloc>
+              ))}
+            </div>
           </div>
         ) : (
           <UpdateIcon className="h-4 w-4 animate-spin" />
         )}
+        <div className="flex justify-end">
+          <Button onClick={() => navigate(-1)} variant="cancel">
+            <ArrowLeft className="mr-2 h-4 w-4" />
+            Quitter
+          </Button>
+        </div>
       </div>
     </div>
   );
