a89a03c2a8c7178afc09f59268dee7deffd54d8b
Timothée <timothee.viardtv@gmail.com>
2024-12-18 17:52:15 +0100

    Fix seed

---
+++ apps/server/prisma/seeds/prod.seed.ts
@@ -0,0 +1,28 @@
+import { db } from "../../src/providers/db";
+import password from "../../src/services/password.service";
+
+async function main() {
+  console.log("Initialisation de la base de données...");
+  // Administrateur
+  await db.user.create({
+    data: {
+      lastName: "Doe",
+      firstName: "John",
+      birthDate: new Date("1990-01-01"),
+      active: true,
+      email: "admin@fastgn.com",
+      password: await password.crypt("root"),
+      gender: "male",
+      phone: "1234567890",
+      admin: {
+        create: {},
+      },
+    },
+  });
+  console.log("Base de données initialisée.");
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
